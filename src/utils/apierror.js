32316b7081374174b0e0b6c513361289a4b9116c
arihant-jain0 <jainari1208@gmail.com>
2023-12-31 17:38:09 +0530

    utils code

---
+++ src/utils/apierror.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = false;
+    this.error =error;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
