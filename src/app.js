bc8bd59e7623106eb58590dae59fe877256b9180
arihant-jain0 <jainari1208@gmail.com>
2024-01-04 15:40:29 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -12,4 +12,14 @@ app.use(express.json({limit:"15kb"}))
 app.use(express.urlencoded({extended:true,limit:"15kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+
+
+import userRouter from "./routes/user.route.js"
+
+/// route delcleartion
+
+app.use("/api/v1/users",userRouter)
+
 export { app }
